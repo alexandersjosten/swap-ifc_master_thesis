743b2fc40d1f04cad2a80997f2a4f4c6e4b63c00
Alexander Sjösten <sjosten.alexander@gmail.com>
2014-10-07 01:09:55 +0200

    cleanup, removed unnecessary stuff

--- prototypes/monadic/Types.hs
+++ prototypes/monadic/Types.hs
@@ -2,30 +2,15 @@
 
 module Types where
 
-import Data.Map
 import Control.Applicative
 import Control.Monad
-import Control.Monad.State
 
 -- | If the level is High, it means private. Low means public.
 data Level = High | Low
 
-type Ident = String
-type Scope a = Map Ident (Level, Term a)
-
--- Internal state, needs to keep track of program counter and the active scopes
--- (perhaps more stuff will be added..)
-data InternalState a = InState { pc :: Level
-                               , activeScopes :: [Scope a]
-                               }
 
 -- | The Flow type keeps track of the information flow.
 newtype Flow level a = Flow (State (level, a) (InternalState a))
-
--- | All the terms are defined within the Term datatype.
-data Term a where
-  Var    :: Term a -> Term a
-  FunApp :: Term (a -> b) -> Term a -> Term b
 
 
 -- Monadic stuff..

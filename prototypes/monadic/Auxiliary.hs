c90ad951c66ac84eef992a2d54d938e17d79e3ed
Alexander Sjösten <sjosten.alexander@gmail.com>
2014-10-16 19:58:51 +0200

    added helper function in Auxiliary for creating high and low flow

--- prototypes/monadic/Auxiliary.hs
+++ prototypes/monadic/Auxiliary.hs
@@ -5,3 +5,11 @@ import Types
 -- | upgrade takes a Flow with Low level and upgrades it
 upgrade :: Flow Low a -> Flow High a
 upgrade (Flow a) = Flow a
+
+-- | creates a Flow with High level given a value
+mkHigh :: a -> Flow High a
+mkHigh = return
+
+-- | creates a Flow with Low level given a value
+mkLow :: a -> Flow Low a
+mkLow = return

e17c725c2c553c580124da7cd4087561d3598d4c
Alexander Sjösten <sjosten.alexander@gmail.com>
2015-02-05 01:18:53 +0100

    changed lprint to use anonymous function, now works with JSFlow

--- src/SwapIFC/Haste/CodeGen.hs
+++ src/SwapIFC/Haste/CodeGen.hs
@@ -6,13 +6,17 @@ import Haste.Foreign
 -- Make it Opaque so it won't be touched or marshalled
 upg :: a -> IO a
 upg = fmap fromOpaque . ffi "upg" . toOpaque
-
+{-
 lprintHaste :: Show a => a -> IO ()
 lprintHaste = p . show
   where
     {-# NOINLINE p #-}
     p :: String -> IO ()
-    p = ffi "lprint"
+    p = ffi "(function(x) { lprint(x); })"
+-}
+
+lprintHaste :: Show a => a -> IO ()
+lprintHaste = ffi "(function(x) { lprint(x); })" . show
 
 declassifyHaste :: IO a -> IO a
 declassifyHaste ioa = do
